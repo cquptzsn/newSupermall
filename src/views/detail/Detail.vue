655020338ca441988756a0619630cf822942f8a2
cquptzsn <1453900701@qq.com>
2020-04-17 21:09:16 +0800

    '完成评论展示'

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -8,6 +8,7 @@
       <detail-shop-info :shop="shop"/>
       <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
       <detail-param-info ref="params" :param-info="paramInfo"/>
+      <detail-comment-info :comment-info="commentInfo"/>
     </scroll>
 
   </div>
@@ -20,6 +21,7 @@
   import DetailShopInfo from './childComps/DetailShopInfo'
   import DetailGoodsInfo from './childComps/DetailGoodsInfo'
   import DetailParamInfo from './childComps/DetailParamInfo'
+  import DetailCommentInfo from './childComps/DetailCommentInfo'
 
   import Scroll from 'components/common/scroll/Scroll'
 
@@ -34,6 +36,7 @@
       DetailShopInfo,
       DetailGoodsInfo,
       DetailParamInfo,
+      DetailCommentInfo,
       Scroll
     },
     data(){
@@ -48,27 +51,32 @@
         commentInfo: {}
       }
     },
-    created(){
+    created() {
       //1.保存传入的iid
-      this.iid = this.$route.params.iid
+      this.iid = this.$route.params.iid;
 
       //2.根据iid请求详情数据
       getDetail(this.iid).then(res => {
         //1.获取顶部轮播数据
-        const data = res.result
-        this.topImages = data.itemInfo.topImages
+        const data = res.result;
+        this.topImages = data.itemInfo.topImages;
 
         //2.获取商品信息
-        this.goods = new Goods(data.itemInfo , data.columns , data.shopInfo.services)
+        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services);
 
         //3.创建店铺信息的对象
-        this.shop = new Shop(data.shopInfo)
+        this.shop = new Shop(data.shopInfo);
 
         //4.获取商品详细信息
-        this.detailInfo = data.detailInfo
+        this.detailInfo = data.detailInfo;
 
         //5.获取商品参数
-        this.paramInfo = new GoodsParam(data.itemParams.info , data.itemParams.rule)
+        this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule);
+
+        //6.获取评论信息
+        if (data.rate.cRate !== 0) {
+          this.commentInfo = data.rate.list[0]
+        }
       })
 
     },
